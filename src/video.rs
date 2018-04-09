7d6cf3422ee10248b714c61c811baadae4e91dad
pravic <ehysta@gmail.com>
2018-04-09 23:11:05 +0300

    fix: C++ vtables must be flat.

--- src/video.rs
+++ src/video.rs
@@ -44,8 +44,17 @@ pub struct iasset {
 /// Video source interface, used by engine to query video state.
 #[repr(C)]
 pub struct video_source_vtbl {
-	pub base: iasset,
+	// <-- iasset:
+	/// Increments the reference count for an interface on an object.
+	pub add_ref: extern "system" fn(this: *mut iasset) -> i32,
 
+	/// Decrements the reference count for an interface on an object.
+	pub release: extern "system" fn(this: *mut iasset) -> i32,
+	// -->
+
+	// <-- video_source
+	/// Retrieves pointers to the supported interfaces on an object.
+	pub get_interface: extern "system" fn(this: *mut iasset, name: *const u8, out: *mut *mut iasset) -> bool,
 	pub play: extern "system" fn(this: *mut video_source) -> bool,
 	pub pause: extern "system" fn(this: *mut video_source) -> bool,
 	pub stop: extern "system" fn(this: *mut video_source) -> bool,
@@ -62,6 +71,7 @@ pub struct video_source_vtbl {
 
 	pub get_balance: extern "system" fn(this: *mut video_source, balance: *mut f64) -> bool,
 	pub set_balance: extern "system" fn(this: *mut video_source, balance: f64) -> bool,
+	// -->
 }
 
 /// Video source interface, used by engine to query video state.
@@ -74,7 +84,17 @@ pub struct video_source {
 /// Video destination interface, represents video rendering site.
 #[repr(C)]
 pub struct video_destination_vtbl {
-	pub base: iasset,
+	// <-- iasset:
+	/// Increments the reference count for an interface on an object.
+	pub add_ref: extern "system" fn(this: *mut iasset) -> i32,
+
+	/// Decrements the reference count for an interface on an object.
+	pub release: extern "system" fn(this: *mut iasset) -> i32,
+	// -->
+
+	// <-- video_destination
+	/// Retrieves pointers to the supported interfaces on an object.
+	pub get_interface: extern "system" fn(this: *mut iasset, name: *const u8, out: *mut *mut iasset) -> bool,
 
 	/// Whether this instance of `video_renderer` is attached to a DOM element and is capable of playing.
 	pub is_alive: extern "system" fn(this: *mut video_destination) -> bool,
@@ -87,6 +107,7 @@ pub struct video_destination_vtbl {
 
 	/// Render the next frame.
 	pub render_frame: extern "system" fn(this: *mut video_destination, data: LPCBYTE, size: UINT) -> bool,
+	// -->
 }
 
 /// Video destination interface, represents video rendering site.
@@ -99,7 +120,30 @@ pub struct video_destination {
 /// Fragmented destination interface, used for partial updates.
 #[repr(C)]
 pub struct fragmented_video_destination_vtbl {
-	pub base: video_destination,
+	// <-- iasset:
+	/// Increments the reference count for an interface on an object.
+	pub add_ref: extern "system" fn(this: *mut iasset) -> i32,
+
+	/// Decrements the reference count for an interface on an object.
+	pub release: extern "system" fn(this: *mut iasset) -> i32,
+	// -->
+
+	// <-- video_destination
+	/// Retrieves pointers to the supported interfaces on an object.
+	pub get_interface: extern "system" fn(this: *mut iasset, name: *const u8, out: *mut *mut iasset) -> bool,
+
+	/// Whether this instance of `video_renderer` is attached to a DOM element and is capable of playing.
+	pub is_alive: extern "system" fn(this: *mut video_destination) -> bool,
+
+	/// Start streaming/rendering.
+	pub start_streaming: extern "system" fn(this: *mut video_destination, frame_width: i32, frame_height: i32, color_space: COLOR_SPACE, src: *const video_source) -> bool,
+
+	/// Stop streaming.
+	pub stop_streaming: extern "system" fn(this: *mut video_destination) -> bool,
+
+	/// Render the next frame.
+	pub render_frame: extern "system" fn(this: *mut video_destination, data: LPCBYTE, size: UINT) -> bool,
+	// -->
 
 	/// Render the specified part of the current frame.
 	pub render_frame_part: extern "system" fn(this: *mut fragmented_video_destination, data: LPCBYTE, size: UINT, x: i32, y: i32, width: i32, height: i32) -> bool,
